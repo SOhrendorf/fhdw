9b76bf45651526fe50673a80f0afc4cf6d8bb41d
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2023-10-24 11:54:21 +0200

    add function: nur natrüliche Zahlen als Eingabe akziptieren

--- src/de/fhdw/bfwd423a/sohre/Demo01/Example08.java
+++ src/de/fhdw/bfwd423a/sohre/Demo01/Example08.java
@@ -2,15 +2,21 @@ package de.fhdw.bfwd423a.sohre.Demo01;
 
 public class Example08 {
     public static void main(String[] args) {
-        System.out.println(factorial(4));
+        System.out.println(factorial( 6.6));
     }
 
-    static int factorial(int number){
-        int result;
-        result = 1;
-        for (int i = 1; i < number; i++){
-            result *= i+1;
+    static double factorial(double number){
+        if(number >= 0 & (number %1 == 0)){
+            int result;
+            result = 1;
+            for (int i = 1; i < number; i++){
+                result *= i+1;
+            }
+            return result;
         }
-        return result;
+        else {
+            System.out.println("Die Eingabe ist keine natürliche Zahl");
+            return number;
+        }
     }
 }
